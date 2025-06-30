228a7578db4370c890c5a631698b5089a72e30fa
Evan GRUCHOT <evangruchot.pro@gmail.com>
2025-06-30 11:28:25 +0200

    fix and update: fix configuration options and installation of react-router

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+} from 'react-router-dom';
+
+function Home() {
+    return <h1>Accueil</h1>;
+}
+
+function About() {
+    return <h1>À propos</h1>;
+}
+
+function NotFound() {
+    return <h1>Page non trouvée</h1>;
+}
 
 function App() {
-    return <h1>Hello React in Symfony!</h1>;
+    return (
+        <BrowserRouter>
+            <nav>
+                <Link to="/">Accueil</Link> | <Link to="/about">À propos</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                {/* Route pour page 404 */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 const root = createRoot(document.getElementById('root'));
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,11 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('base.html.twig');
